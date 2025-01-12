3434644e13022a629c3bf4b281ff91f45364a8b0
vedataltun <rttvedudi@gmail.com>
2025-01-12 18:37:38 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import "./global.css"
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/deneme.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const deneme = () => {
+  return (
+    <View>
+      <Text>deneme</Text>
+    </View>
+  );
+};
+
+export default deneme;
+
+const styles = StyleSheet.create({});
